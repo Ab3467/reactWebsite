1c6ee3f1900733edf2b32e0fa52fa18d5340351c
Ahmad Abu Bakr <138695838+Ab3467@users.noreply.github.com>
2023-08-31 15:31:32 +0500

    commit

---
+++ my-app/src/Pages/Home/Homescreen/index.jsx
@@ -0,0 +1,14 @@
+import HeroSection from "../HeroSection";
+import MySkills from "../MySkills";
+import AboutMe from "../Aboutme"
+
+
+export const Home = () => {
+  return (
+    <>
+    <HeroSection/>
+    <MySkills/>
+    <AboutMe/>
+    </>
+  )
+}
